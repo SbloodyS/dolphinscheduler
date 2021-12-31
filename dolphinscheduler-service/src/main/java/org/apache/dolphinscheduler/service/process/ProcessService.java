61296756bb07859aae6da50bf06bee0e9325d63a
SbloodyS <sbloodys@qq.com>
2021-12-31 16:43:37 +0800

    fix bug

--- dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/process/ProcessService.java
+++ dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/process/ProcessService.java
@@ -989,11 +989,12 @@ public class ProcessService {
 
         List<Property> parentPropertyList = JSONUtils.toList(parentGlobalParams, Property.class);
         List<Property> subPropertyList = JSONUtils.toList(subGlobalParams, Property.class);
+        subPropertyList = new ArrayList<>(subPropertyList);
 
         Map<String, String> subMap = subPropertyList.stream().collect(Collectors.toMap(Property::getProp, Property::getValue));
 
         for (Property parent : parentPropertyList) {
-            if (subMap.containsKey(parent.getProp())) {
+            if (!subMap.containsKey(parent.getProp())) {
                 subPropertyList.add(parent);
             }
         }
