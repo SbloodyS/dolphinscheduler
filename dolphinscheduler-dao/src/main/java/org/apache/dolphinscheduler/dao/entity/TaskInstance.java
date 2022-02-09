687bf5572ca0e6a227a786d6c6569b5e3defe33a
SbloodyS <460888207@qq.com>
2022-02-09 22:50:41 +0800

    test

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
@@ -588,7 +588,7 @@ public class TaskInstance implements Serializable {
         }
         if (this.getState() == ExecutionStatus.NEED_FAULT_TOLERANCE) {
             return true;
-        } else if (processInstance.getState() == ExecutionStatus.READY_STOP) {
+        } else if (this.getState() == ExecutionStatus.READY_STOP) {
             return false;
         } else {
             return (this.getState().typeIsFailure()
