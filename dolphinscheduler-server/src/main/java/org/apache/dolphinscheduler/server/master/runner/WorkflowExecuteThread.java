c5141c308d845329b8b1696043c57a49f32158ce
SbloodyS <460888207@qq.com>
2022-02-10 15:44:37 +0800

    test

--- dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteThread.java
+++ dolphinscheduler-server/src/main/java/org/apache/dolphinscheduler/server/master/runner/WorkflowExecuteThread.java
@@ -1093,7 +1093,7 @@ public class WorkflowExecuteThread implements Runnable {
 
         if ((activeTaskProcessorMaps.size() > 0 && state != ExecutionStatus.READY_STOP) || hasRetryTaskInStandBy()) {
             // active task and retry task exists
-            logger.info("{} in runningState, readyToSubmitTaskQueue: {}", instance.getId(), readyToSubmitTaskQueue.toString());
+            logger.info("{} in runningState, readyToSubmitTaskQueueSize: {}", instance.getId(), readyToSubmitTaskQueue.size());
             return runningState(state);
         }
         // process failure
